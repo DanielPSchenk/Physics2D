760c4de52d2ac34d07b43410abc37cd132878027
Daniel Schenk <102045363+DanielPSchenk@users.noreply.github.com>
2023-08-24 11:25:34 +0200

    cascade controller

--- Scripts/PendulumController.cs
+++ Scripts/PendulumController.cs
@@ -10,21 +10,35 @@ public class PendulumController : Effector
     public PhysicsPart pendulum;
 
     private Pid p;
+
+    public float wOuter;
+    public float wInner;
+
+    public float ArcsinApproximation(float x)
+    {
+        return x + x * x * x / 6;
+    }
     
-    // Start is called before the first frame update
     public override void ApplyForce(UnityEngine.Vector3[][] stateVector, UnityEngine.Vector3[] secondDerivativeVector)
     {
         //PID control
         //AddForce(b, new Vector2(p.NextU(phi()),0), secondDerivativeVector);
         
         //IO-linearization control
+        //outer controller
+
+        wInner = ((wOuter - .2f * stateVector[1][pendulum.id].x - 1f * stateVector[0][pendulum.id].x) / .01f / 5);
+
+        
+
+        //inner controller
         var relMov = stateVector[1][pendulum.id] - stateVector[1][b.id];
         Vector2 con = (stateVector[0][pendulum.id] - stateVector[0][b.id]);
         Vector2 direction = con.normalized;
         Vector2 tangent = new Vector2(direction.y, -direction.x);
         float phiDerivative = Vector2.Dot(tangent, relMov) / con.magnitude;
         float p = phi();
-        float u = (9.81f * 25 * Mathf.Sin(p) + 50f * phiDerivative + 625f * p) / (2.5f * Mathf.Cos(p));
+        float u = (9.81f * 25 * Mathf.Sin(p) + 100f * phiDerivative + 2500f * p - wInner) / (2.5f * Mathf.Cos(p));
         
         AddForce(b, new Vector2(u, 0), secondDerivativeVector);
     }
